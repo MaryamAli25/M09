acb4ee39058d1ea2a63f8d76cb7e390bb6a52839
MaryamAli25 <maryam.aladda@gmail.com>
2025-03-18 20:30:56 -0400

    Finalize demo package with README

---
+++ README.md
@@ -0,0 +1,3 @@
+# M09 Challenge - Demo Package
+
+This repository contains a simple Python package named `demo` with a function `tell_me_something()` that prints message.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='1.0.0',
+    url='https://github.com/MaryamAli25/M09',
+    author='Maryam Ali',
+    author_email='maryam.aladda@gmail.com',
+    description='A simple demo.',
+    packages=['demo'],
+    install_requires=[],
+)
